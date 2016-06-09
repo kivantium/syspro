112a92e7afbece1786f2f8583f31d9d5ef53577d
kivantium <kivantium@gmail.com>
2016-06-09 11:43:20 +0900

    implement multiple pipe ignoring big stream

--- report6/main.c
+++ report6/main.c
@@ -8,67 +8,58 @@
 #include <fcntl.h>
 #include "parse.h"
 
-int exec_process(process* pr, char *envp[]) {
-    int status, stdin_old, stdout_old;
-    pid_t pid;
-
-    if(pr->program_name == NULL) {
-        return -1;
-    }
-
-    if(pr->input_redirection != NULL) {
-        int fd;
-        stdin_old = dup(STDIN_FILENO);
-        if((fd = open(pr->input_redirection, O_RDONLY, 0666)) < 0) {
-            perror("error opening input file");
-            return 0;
-        }
-        dup2(fd, STDIN_FILENO);
-        close(fd);
-    }
-
-    if (pr->output_redirection != NULL) {
-        int fd;
-        stdout_old = dup(STDOUT_FILENO);
-        if(pr->output_option == TRUNC) 
-            fd = open(pr->output_redirection, O_CREAT | O_WRONLY, 0666);
-        else
-            fd = open(pr->output_redirection, O_CREAT | O_WRONLY | O_APPEND, 0666);
-        dup2(fd, STDOUT_FILENO);
-        close(fd);
-    }
-
-    if(pr->argument_list != NULL) {
-        if((pid = fork()) == 0) {
-            if(execve(pr->argument_list[0], pr->argument_list, envp) < 0) {
-                perror("error executing command");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            waitpid(pid, &status, WUNTRACED );
-            dup2(stdin_old, STDIN_FILENO);
-            dup2(stdout_old, STDOUT_FILENO);
-        }
-    }
-    return 0;
-}
-
 int main(int argc, char *argv[], char *envp[]) {
     char s[LINELEN];
     job *curr_job;
     job*     jb;
     process* pr;
+    pid_t pid;
 
     while(get_line(s, LINELEN)) {
         if(!strcmp(s, "exit\n")) break;
         curr_job = parse_line(s);
 
         for(jb = curr_job; jb != NULL; jb = jb->next) {
-            for(pr = jb->process_list; pr != NULL; pr = pr->next) {
-                if(exec_process(pr, envp) < 0) exit(EXIT_FAILURE);
-
-                if(jb->next != NULL)  printf("\n");
+            int p[2];
+            pid_t pid;
+            int fd_in = 0;
+            pr = jb->process_list;
+            if(pr->input_redirection != NULL) {
+                if((fd_in = open(pr->input_redirection, O_RDONLY, 0666)) < 0) {
+                    perror("error opening input file");
+                    return 0;
+                }
             }
+            while(pr != NULL) {
+                pipe(p);
+                if((pid = fork()) < 0) {
+                    perror("error in fork");
+                    exit(EXIT_FAILURE);
+                } else if(pid == 0) {
+                    dup2(fd_in, 0);
+                    if(pr->next != NULL) dup2(p[1], 1);
+                    else if(pr->output_redirection != NULL){
+                        int fd;
+                        if(pr->output_option == TRUNC) 
+                            fd = open(pr->output_redirection, O_CREAT | O_WRONLY, 0666);
+                        else
+                            fd = open(pr->output_redirection, O_CREAT | O_WRONLY | O_APPEND, 0666);
+                        dup2(fd, 1);
+                        close(fd);
+                    }
+                    close(p[0]);
+                    if(execve(pr->argument_list[0], pr->argument_list, envp) < 0) {
+                        perror("error executing command");
+                        exit(EXIT_FAILURE);
+                    }
+                } else {
+                    close(p[1]);
+                    fd_in = p[0];
+                    pr = pr->next;
+                }
+            }
+            wait(NULL);
+            if(jb->next != NULL)  printf("\n");
         }
         free_job(curr_job);
     }

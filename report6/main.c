2fd9b916f3ee768c9cd3d34145cd6927130f3e08
kivantium <kivantium@gmail.com>
2016-06-09 21:36:00 +0900

    maybe complete

--- report6/main.c
+++ report6/main.c
@@ -20,45 +20,68 @@ int main(int argc, char *argv[], char *envp[]) {
         curr_job = parse_line(s);
 
         for(jb = curr_job; jb != NULL; jb = jb->next) {
-            int p[2];
+            int n = 0;
+            int old_fd[2], new_fd[2];
             pid_t pid;
-            int fd_in = 0;
             pr = jb->process_list;
-            if(pr->input_redirection != NULL) {
-                if((fd_in = open(pr->input_redirection, O_RDONLY, 0666)) < 0) {
-                    perror("error opening input file");
-                    return 0;
+            for(;pr != NULL; pr=pr->next) n++;
+            pr = jb->process_list;
+            for(int i=0; pr != NULL; pr=pr->next, ++i) {
+                if(pr->next != NULL) {
+                    pipe(new_fd);
                 }
-            }
-            while(pr != NULL) {
-                pipe(p);
+
                 if((pid = fork()) < 0) {
                     perror("error in fork");
                     exit(EXIT_FAILURE);
                 } else if(pid == 0) {
-                    dup2(fd_in, 0);
-                    if(pr->next != NULL) dup2(p[1], 1);
-                    else if(pr->output_redirection != NULL){
+                    if(i > 0) {
+                        dup2(old_fd[0], 0);
+                        close(old_fd[0]);
+                        close(old_fd[1]);
+                    }
+                    if(pr->next != NULL) {
+                        close(new_fd[0]);
+                        dup2(new_fd[1], 1);
+                        close(new_fd[1]);
+                    }
+                    if(i==0 && pr->input_redirection != NULL) {
+                        int fd;
+                        if((fd = open(pr->input_redirection, O_RDONLY,
+                                        0666)) < 0) {
+                            perror("error opening input file");
+                            return 0;
+                        }
+                        dup2(fd, 0);
+                    }
+                    if(pr->next == NULL && pr->output_redirection != NULL){
                         int fd;
                         if(pr->output_option == TRUNC) 
-                            fd = open(pr->output_redirection, O_CREAT | O_WRONLY, 0666);
+                            fd = open(pr->output_redirection, O_CREAT | O_WRONLY | O_TRUNC, 0666);
                         else
                             fd = open(pr->output_redirection, O_CREAT | O_WRONLY | O_APPEND, 0666);
                         dup2(fd, 1);
-                        close(fd);
                     }
-                    close(p[0]);
                     if(execve(pr->argument_list[0], pr->argument_list, envp) < 0) {
                         perror("error executing command");
                         exit(EXIT_FAILURE);
                     }
                 } else {
-                    close(p[1]);
-                    fd_in = p[0];
-                    pr = pr->next;
+                    if(i > 0) {
+                        close(old_fd[0]);
+                        close(old_fd[1]);
+                    }
+                    if(pr->next != NULL) {
+                        old_fd[0] = new_fd[0];
+                        old_fd[1] = new_fd[1];
+                    }
                 }
             }
-            wait(NULL);
+            if (n > 1) {
+                close(old_fd[0]);
+                close(old_fd[1]);
+            }
+            for(int i=0; i<2*n; ++i) wait(NULL);
             if(jb->next != NULL)  printf("\n");
         }
         free_job(curr_job);
